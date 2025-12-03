75a5e9b0358ea4d619b0612da9ff47b8aef99913
toddming <toddming@qq.com>
2025-12-03 17:21:48 +0800

    Signed-off-by: toddming <toddming@qq.com>

--- src/DateTimeInputInterface.cpp
+++ src/DateTimeInputInterface.cpp
@@ -1,9 +1,11 @@
 ﻿#include "DateTimeInputInterface.h"
 
+#include "QFluent/date_time/DatePicker.h"
+#include "QFluent/date_time/TimePicker.h"
 #include "QFluent/date_time/CalendarPicker.h"
 
 DateTimeInputInterface::DateTimeInputInterface(QWidget *parent)
-    : GalleryInterface("基本输入", "", parent)
+    : GalleryInterface("日期和时间", "", parent)
 {
     setObjectName("DateTimeInputInterface");
 
@@ -12,4 +14,8 @@ DateTimeInputInterface::DateTimeInputInterface(QWidget *parent)
     auto calendarView = new CalendarView(this);
     calendarView->setDisabled(true);
     addExampleCard("流畅日历", calendarView);
+    addExampleCard("日期选择器", new ZhDatePicker(this));
+    addExampleCard("时间选择器", new AMTimePicker(this));
+    addExampleCard("24小时制的时间选择器", new TimePicker(this, false));
+    addExampleCard("显示秒的时间选择器", new TimePicker(this, true));
 }
